54a69b21c4f7148296c40e57cc77c3410246c343
yuanlida <lida@edison.tech>
2020-03-16 09:44:16 +0800

    char cnn

--- char_cnn/char_cnn_edo.py
+++ char_cnn/char_cnn_edo.py
@@ -48,10 +48,10 @@ class ModelConfig(object):
 
 
 class Config(object):
-    alphabet = "abcdefghijklmnopqrstuvwxyz0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-,;.!?:'\"/\\|_@#$%^&*~`+-=<>()[]{}"
     #     alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
 
-    sequenceLength = 500
+    sequenceLength = 200
     batchSize = 128
 
     numClasses = 5  # 二分类设置为1，多分类设置为类别的数目
@@ -156,7 +156,7 @@ class Dataset(object):
 
         trainIndex = int(len(x) * rate)
 
-        datas = list(zip(reviews, y))
+        datas = list(zip(reviews, labels))
         random.shuffle(datas)
         reviews = []
         y = []
