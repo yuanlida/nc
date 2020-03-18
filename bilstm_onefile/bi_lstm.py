569d9de5e247fdd14cb0117f05560a6571c2a8b7
yuanlida <lida@edison.tech>
2020-03-19 07:54:57 +0800

    change to train data.

--- bilstm_onefile/bi_lstm.py
+++ bilstm_onefile/bi_lstm.py
@@ -37,7 +37,7 @@ from tensorflow.lite.experimental.examples.lstm.rnn import bidirectional_dynamic
 # 配置参数
 
 class TrainingConfig(object):
-    epoches = 1
+    epoches = 10
     evaluateEvery = 100
     checkpointEvery = 100
     learningRate = 0.001
