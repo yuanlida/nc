f772fceafb168867f0b826abc533b268f556e85b
yuanlida <lida@edison.tech>
2020-03-18 20:42:24 +0800

    tmp

--- tf_converter.py
+++ tf_converter.py
@@ -27,8 +27,10 @@ import tensorflow as tf
 converter = tf.compat.v1.lite.TFLiteConverter.from_saved_model("./model/Bi-LSTM/savedModel", tag_set=[tf.saved_model.tag_constants.SERVING])
 # converter.target_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
 #                         tf.lite.OpsSet.SELECT_TF_OPS]
-converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
-                                       tf.lite.OpsSet.SELECT_TF_OPS]
+
+# converter.change_concat_input_ranges = False
+# converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS,
+#                                        tf.lite.OpsSet.SELECT_TF_OPS]
 
 tflite_model = converter.convert()
 open("./model/Bi-LSTM/bi-lstm.tflite", "wb").write(tflite_model)
