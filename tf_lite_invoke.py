d2ef5da10e34d1484d1981795ae96606ba4db699
yuanlida <lida@edison.tech>
2020-03-20 17:10:54 +0800

    invoke tf lite

--- tf_lite_invoke.py
+++ tf_lite_invoke.py
@@ -12,13 +12,25 @@ print(input_details)
 print(output_details)
 
 # Test model on random input data.
-# input_shape = input_details[0]['shape']
-# input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-# interpreter.set_tensor(input_details[0]['index'], input_data)
-#
-# interpreter.invoke()
-#
-# # The function `get_tensor()` returns a copy of the tensor data.
-# # Use `tensor()` in order to get a pointer to the tensor.
-# output_data = interpreter.get_tensor(output_details[0]['index'])
-# print(output_data)
+input_shape = input_details[0]['shape']
+input_data = np.array(np.random.random_integers(0, 0, size=input_shape), dtype=np.int32)
+print(input_data)
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+input_shape = input_details[1]['shape']
+input_data = np.array(np.random.random_integers(0, 0, size=input_shape), dtype=np.float32)
+print(input_data)
+interpreter.set_tensor(input_details[1]['index'], input_data)
+
+input_shape = input_details[2]['shape']
+input_data = np.array(np.random.random_integers(0, 0, size=input_shape), dtype=np.int32)
+print(input_data)
+interpreter.set_tensor(input_details[2]['index'], input_data)
+
+
+interpreter.invoke()
+
+# The function `get_tensor()` returns a copy of the tensor data.
+# Use `tensor()` in order to get a pointer to the tensor.
+output_data = interpreter.get_tensor(output_details[0]['index'])
+print(output_data)
