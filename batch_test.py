acd313e20b79655424fbbb311840c4b8ac15e045
jeffreyhalim <freshiefrey@github.com>
2020-03-26 04:11:27 +0800

    data

--- batch_test.py
+++ batch_test.py
@@ -16,15 +16,22 @@ if __name__ == '__main__':
     for file_name in test_files:
         filepath, tmpfilename = os.path.split(file_name)
         shotname, extension = os.path.splitext(tmpfilename)
+        error = 0
         # print(shotname)
         with open(error_file + shotname + '.txt', 'w') as of:
             with open(file_name) as f:
+                total = len(f.readlines())
+                # print(total)
                 for line in f.readlines():
                     lite_model.set_sentence(line)
                     label = lite_model.analyze()
                     if label != shotname:
-                        print(line)
+                        error += 1
+                        # print(line)
                         of.write(line.strip())
                         of.write('\t')
                         of.write(label)
                         of.write('\n')
+                        # print(error)
+                acc = (total - error)/ total
+                print(shotname, 'accuracy:', str(acc))
